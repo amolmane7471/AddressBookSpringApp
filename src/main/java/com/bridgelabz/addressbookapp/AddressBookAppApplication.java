70efedb743c279b965bf12cab85f46beaa7f9914
amolmane7471 <maneamol3233@gmail.com>
2022-11-06 10:25:57 +0530

    [Amol] : added UC1 create AddressBook Spring Application to Rest request from adrressBook UI

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -2,12 +2,14 @@ package com.bridgelabz.addressbookapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
 public class AddressBookAppApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(AddressBookAppApplication.class, args);
+        System.out.println("Welcome To Address Book App");
     }
 
 }
