75928a50dae9dc7e49f702e09825944502d347bd
amolmane7471 <maneamol3233@gmail.com>
2022-11-08 13:45:19 +0530

    [Amol] : added section3 UC3 determine the logging levels

--- src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
+++ src/main/java/com/bridgelabz/addressbookapp/AddressBookAppApplication.java
@@ -3,6 +3,7 @@ package com.bridgelabz.addressbookapp;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 import org.springframework.http.converter.json.GsonBuilderUtils;
 
 @SpringBootApplication
@@ -10,8 +11,9 @@ import org.springframework.http.converter.json.GsonBuilderUtils;
 public class AddressBookAppApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(AddressBookAppApplication.class, args);
-        log.info("AddressBook App Started");
+        ApplicationContext context = SpringApplication.run(AddressBookAppApplication.class, args);
+
+        log.info("Address Book App Started in {} Environment",context.getEnvironment().getProperty("environment"));
     }
 
 }
