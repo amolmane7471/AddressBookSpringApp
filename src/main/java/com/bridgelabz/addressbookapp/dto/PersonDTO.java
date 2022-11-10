3133abb00954f326b7534b82f542e2d1e27b6e19
amolmane7471 <maneamol3233@gmail.com>
2022-11-10 19:28:31 +0530

    [Amol] : added section4 UC2 provide user friendly error response

--- src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
@@ -2,21 +2,25 @@ package com.bridgelabz.addressbookapp.dto;
 import lombok.Data;
 import lombok.ToString;
 import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Pattern;
 
 @Data
 @ToString
 public class PersonDTO {
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "First name Invalid")
 
     @NotEmpty(message = "Should Not be Empty")
     public String firstName;
+    @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Last name Invalid")
 
     @NotEmpty(message = "Should Not be Empty")
     public String lastName;
-
+    @Pattern(regexp = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$", message = "Email is Not Valid")
     @NotEmpty(message = "Should Not be Empty")
     public String email;
     @NotEmpty(message = "Should Not be Empty")
     public String address;
+    @Pattern(regexp = "^[0-9]{2}[\\s][0-9]{10}$",message = "Invalid Mobile Number")
     @NotEmpty(message = "Should Not be Empty")
     public String contact;
     public PersonDTO(String firstName,String lastName, String email, String address, String contact) {
