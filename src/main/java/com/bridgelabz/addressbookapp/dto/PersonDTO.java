ae673c62ba636fbc7bd1c6f0a7d56f5fe3627fa7
amolmane7471 <maneamol3233@gmail.com>
2022-11-10 21:31:56 +0530

    [Amol] : added section5 UC1 Added remaining properties to the person DTO and Model

--- src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
+++ src/main/java/com/bridgelabz/addressbookapp/dto/PersonDTO.java
@@ -5,8 +5,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 @Data
-@ToString
-public class PersonDTO {
+public @ToString class PersonDTO {
     @Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "First name Invalid")
 
     @NotEmpty(message = "Should Not be Empty")
@@ -15,20 +14,28 @@ public class PersonDTO {
 
     @NotEmpty(message = "Should Not be Empty")
     public String lastName;
-    @Pattern(regexp = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$", message = "Email is Not Valid")
-    @NotEmpty(message = "Should Not be Empty")
-    public String email;
     @NotEmpty(message = "Should Not be Empty")
     public String address;
+    public String city;
+
+    public String state;
+
+    public String pin;
     @Pattern(regexp = "^[0-9]{2}[\\s][0-9]{10}$",message = "Invalid Mobile Number")
     @NotEmpty(message = "Should Not be Empty")
     public String contact;
-    public PersonDTO(String firstName,String lastName, String email, String address, String contact) {
+    @Pattern(regexp = "^[a-zA-Z]+[a-zA-Z0-9]*[- . + _]?[a-zA-Z0-9]+[@]{1}[a-z0-9]+[.]{1}[a-z]+[.]?[a-z]+$", message = "Email is Not Valid")
+    @NotEmpty(message = "Should Not be Empty")
+    public String email;
+    public PersonDTO(String firstName,String lastName,String address,String city,String state,String pin, String contact,String email) {
         this.firstName = firstName;
         this.lastName = lastName;
-        this.email = email;
+        this.city = city;
+        this.state = state;
+        this.pin = pin;
         this.address = address;
         this.contact = contact;
+        this.email = email;
     }
 
 }
--- src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
+++ src/main/java/com/bridgelabz/addressbookapp/model/AddressBookData.java
@@ -1,72 +1,30 @@
 package com.bridgelabz.addressbookapp.model;
 
 import com.bridgelabz.addressbookapp.dto.PersonDTO;
+import lombok.Data;
 
 
-public class AddressBookData {
+public @Data class AddressBookData {
     private int personId;
     private String firstName;
     private String lastName;
-    private String email;
     private String address;
+    private String city;
+    private String state;
+    private String pin;
     private String contact;
+    private String email;
 
     public AddressBookData(){ }
     public AddressBookData(int personId, PersonDTO personDTO) {
     this.personId = personId;
     this.firstName = personDTO.firstName;
     this.lastName = personDTO.lastName;
-    this.email = personDTO.email;
+    this.city = personDTO.city;
+    this.state = personDTO.state;
+    this.pin = personDTO.pin;
     this.address = personDTO.address;
     this.contact = personDTO.contact;
-
-    }
-
-    public int getPersonId() {
-        return personId;
-    }
-
-    public void setPersonId(int personId) {
-        this.personId = personId;
-    }
-
-    public String getFirstName() {
-        return firstName;
-    }
-
-    public void setFirstName(String firstName) {
-        this.firstName = firstName;
-    }
-
-    public String getLastName() {
-        return lastName;
-    }
-
-    public void setLastName(String lastName) {
-        this.lastName = lastName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getAddress() {
-        return address;
-    }
-
-    public void setAddress(String address) {
-        this.address = address;
-    }
-
-    public String getContact() {
-        return contact;
-    }
-
-    public void setContact(String contact) {
-        this.contact = contact;
+    this.email = personDTO.email;
     }
 }
